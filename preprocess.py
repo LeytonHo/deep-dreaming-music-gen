08085e6eccaa551447e2944a11735a52a4c03e3c
LeytonHo <leyton.ho@gmail.com>
2021-11-23 23:01:29 -0500

    Get track genre from metadata

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import librosa
 import os
 import numpy as np
 import warnings
+import utils
 
 def preprocess(rootdir):
     """
@@ -15,20 +16,30 @@ def preprocess(rootdir):
     sr_data = []
     genre_data = []
 
+    # https://notebook.community/mdeff/fma/usage
+    tracks = utils.load('fma_metadata/tracks.csv')
+    small = tracks[tracks['set', 'subset'] <= 'small']
+    track_id_to_genre = small['track', 'genres'].to_dict()
+
     for subdir, _, files in os.walk(rootdir):
         for file in files:
             if file.endswith('.mp3'):
                 try:
                     filepath = subdir + '/' + file
+                    track_id = int(os.path.splitext(file)[0])
+                    genre = track_id_to_genre[track_id]
+                    if len(genre) > 1:
+                        continue
                     audio, sr = librosa.load(filepath)
                     audio_data += [audio]
                     sr_data += [sr]
+                    genre_data += [genre]
 
                 except RuntimeError:
                     # Ignore malformed mp3 files
                     continue
 
-    return audio_data, sr_data
+    return audio_data, sr_data, genre_data
 
 # Suppress expected warnings from librosa, source: https://github.com/librosa/librosa/issues/1015#issuecomment-552506963
 with warnings.catch_warnings():

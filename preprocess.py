688e544677eb6e553fb531b54f3fbb8f59336cc7
LeytonHo <leyton.ho@gmail.com>
2021-11-23 23:54:30 -0500

    Add comments

--- preprocess.py
+++ preprocess.py
@@ -10,16 +10,16 @@ def preprocess(rootdir):
     Inputs:
     - rootdir: Directory housing input mp3 files
     Returns:
-    - audio_data: Vectorized audio data
-    - sr_data: Sampling rate for each track
+    - audio_data: Vectorized audio data as amplitude of waveform at each t in time series
+    - sr_data: Sampling rate of each track
     - genre_data: Genre of each track
     """
-    truncated_size = 660984 # Sizes of audio files are 660984 and 661560. Truncating to 660984 for simplicity
+    truncated_size = 660984 # Sizes of audio files are 660984 and 661560 (time series points). Truncating to 660984 for simplicity.
     audio_data = np.empty((truncated_size,0))
     sr_data = np.array([])
     genre_data = np.array([])
 
-    # https://notebook.community/mdeff/fma/usage
+    # Reference: https://notebook.community/mdeff/fma/usage
     tracks = utils.load('fma_metadata/tracks.csv')
     small = tracks[tracks['set', 'subset'] <= 'small']
     track_id_to_genre = small['track', 'genres'].to_dict()
@@ -31,8 +31,11 @@ def preprocess(rootdir):
                     filepath = subdir + '/' + file
                     track_id = int(os.path.splitext(file)[0])
                     genre = track_id_to_genre[track_id]
+
+                    # Don't process multi-genre tracks
                     if len(genre) > 1:
                         continue
+
                     audio, sr = librosa.load(filepath)
                     audio_data = np.append(audio_data, np.reshape(audio[:truncated_size], (truncated_size, 1)), axis=1)
                     sr_data = np.append(sr_data, sr)

8e0f5fb8d230db833dd164fad6be7ce304373bf6
Kevin Cox <kevindanielcox@gmail.com>
2021-12-07 15:15:13 -0500

    Imports

--- genre_switcher.py
+++ genre_switcher.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import numpy as np
+import pickle
 
 # TODO:
 # - Check that encoder, decoder, classifier are not being trained by the genre switcher
@@ -46,7 +48,34 @@ class GenreSwitcher(tf.keras.Model):
             loss = self.loss(classification)
 
 def main():
-    tf.keras.models.load_model('saved_models/saved_model.pb') 
+    autoencoder = tf.keras.models.load_model('saved_models/autoencoder_to_delete', compile=False) 
+    classifier = tf.keras.models.load_model('saved_models/classifier', compile=False)
+
+
+    # genre_switcher = GenreSwitcher(classifier, autoencoder, 0)
+
+    ### LOAD DATA #############################################
+    with open('preprocessed.pickle', 'rb') as f:
+        audio_data, sr_data, genre_data = pickle.load(f)
+
+    print(np.shape(audio_data))
+    audio_data = np.reshape(audio_data, (np.shape(audio_data)[0], np.shape(audio_data)[1], 1))
+    print(np.shape(audio_data))
+    total_tracks = np.shape(audio_data)[0]
+    train_tracks = int(total_tracks * 2 / 3)
+    print(train_tracks)
+    x_train = audio_data[:train_tracks]
+    x_test = audio_data[train_tracks:]
+    ###
+    SMOL = 10
+    x_train = x_train[:SMOL]
+    x_test = x_test[:SMOL]
+    ###
+    #############################################################
+    # print(autoencoder.encoder(x_train))
+    autoencoder.compile(optimizer='adam', loss=tf.keras.losses.MeanSquaredError())
+    # autoencoder.build(np.shape(x_train))
+    autoencoder.call(x_train)
 
 if __name__ == "__main__":
     main()

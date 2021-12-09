ad2f9c2f7481000a6a21b9aa2179d4c7052dd5bc
Kevin Cox <kevindanielcox@gmail.com>
2021-12-09 17:48:11 -0500

    Revise documentation

--- genre_switcher.py
+++ genre_switcher.py
@@ -2,10 +2,6 @@ import tensorflow as tf
 import numpy as np
 import pickle
 
-# TODO:
-# - Check that encoder, decoder, classifier are not being trained by the genre switcher
-# - Check that the loss calculation is correct (correct loss? should be lists?)
-
 
 class GenreSwitcher(tf.keras.Model):
     def __init__(self, classifier, autoencoder, desired_classification, input, learning_rate=0.01, num_epochs=1):
@@ -19,7 +15,7 @@ class GenreSwitcher(tf.keras.Model):
 
         self.latent_vector = tf.Variable(self.autoencoder.encoder(input))
 
-        # fix model weights
+        # hold model weights fixed
         self.classifier.trainable = False
         self.autoencoder.trainable = False
 
@@ -46,40 +42,3 @@ class GenreSwitcher(tf.keras.Model):
 
             gradients = tape.gradient(loss, self.trainable_variables)
             self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
-
-            # stop training if desired classification is reached
-
-def main():
-    # autoencoder = tf.keras.models.load_model('saved_models/autoencoder_to_delete', compile=False) 
-    # classifier = tf.keras.models.load_model('saved_models/classifier', compile=False)
-
-
-    # # genre_switcher = GenreSwitcher(classifier, autoencoder, 0)
-
-    # ### LOAD DATA #############################################
-    # with open('preprocessed.pickle', 'rb') as f:
-    #     audio_data, sr_data, genre_data = pickle.load(f)
-
-    # print(np.shape(audio_data))
-    # audio_data = np.reshape(audio_data, (np.shape(audio_data)[0], np.shape(audio_data)[1], 1))
-    # print(np.shape(audio_data))
-    # total_tracks = np.shape(audio_data)[0]
-    # train_tracks = int(total_tracks * 2 / 3)
-    # print(train_tracks)
-    # x_train = audio_data[:train_tracks]
-    # x_test = audio_data[train_tracks:]
-    # ###
-    # SMOL = 10
-    # x_train = x_train[:SMOL]
-    # x_test = x_test[:SMOL]
-    # ###
-    # #############################################################
-    # # print(autoencoder.encoder(x_train))
-    # autoencoder.compile(optimizer='adam', loss=tf.keras.losses.MeanSquaredError())
-    # # autoencoder.build(np.shape(x_train))
-    # autoencoder.call(x_train)
-    pass
-
-if __name__ == "__main__":
-    main()
-
